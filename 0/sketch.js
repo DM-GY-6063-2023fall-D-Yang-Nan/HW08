36f2024e7f0a428c3a941a08128c118064a4b58c
NanYangEvelyn <ny2187@nyu.edu>
2023-10-29 19:07:59 -0400

    code1 finished

--- 0/sketch.js
+++ 0/sketch.js
@@ -1,12 +1,63 @@
+let origImg;
+let img;
+let replacementImg;
+let cp;
+
+let xOff;
+let yOff;
+
+function preload() {
+  origImg = loadImage('picture.jpg');  // 从项目目录加载 Mondrian 图像
+  replacementImg = loadImage('111.jpg');  // 加载要替换的图像
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  origImg.resize(0, height);  // 调整原始图像的大小以匹配浏览器窗口高度
+  img = createImage(origImg.width, origImg.height);  // 创建一个新图像对象来存储原始图像的副本
+  
+  replacementImg.resize(origImg.width, origImg.height);  // 调整替换图像的大小以匹配原始图像的大小
+  
+  cp = createColorPicker("#ff0000");  // 创建一个初始颜色为红色的颜色选择器
+  cp.position(10, 10);  // 设置颜色选择器的位置
+
+   if (img.width > width) {
+     img.resize(width, 0);
+     }
+     if (img.height > height) {
+     img.resize(0, height);
+     }
+    
+     xOff = (width - img.width) / 2;
+     yOff = (height - img.height) / 2;
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2.5, height / 4, width / 20, height / 2);
-  rect(width - width / 2.5 - width / 20, height / 4, width / 20, height / 2);
-  rect(width / 2.5 + width / 20, height / 4 - width / 20, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
-  rect(width / 2.5 + width / 20, height / 4 + height / 2, width - 2 * width / 2.5 - 2 * width / 20, width / 20);
+  img.copy(origImg, 0, 0, origImg.width, origImg.height, 0, 0, origImg.width, origImg.height);  // 将原始图像复制到新图像对象中
+  
+  let newColor = cp.color();  // 获取颜色选择器的当前颜色
+  
+  img.loadPixels();
+  replacementImg.loadPixels();  // 加载替换图像的像素数据
+  
+  for (let i = 0; i < img.pixels.length; i += 4) {
+    let r = img.pixels[i];
+    let g = img.pixels[i+1];
+    let b = img.pixels[i+2];
+    
+    if (r > 80 && g < 100 && b < 100) {  // 调整后的红色检测逻辑
+      img.pixels[i] = red(newColor);   // 替换红色通道
+      img.pixels[i+1] = green(newColor);  // 替换绿色通道
+      img.pixels[i+2] = blue(newColor);  // 替换蓝色通道
+    } else if (r < 100 && g < 100 && b > 90) {  // 蓝色检测逻辑
+      img.pixels[i] = replacementImg.pixels[i];   // 替换图像的红色通道
+      img.pixels[i+1] = replacementImg.pixels[i+1];  // 替换图像的绿色通道
+      img.pixels[i+2] = replacementImg.pixels[i+2];  // 替换图像的蓝色通道
+    }
+  }
+  
+  img.updatePixels();
+  translate(xOff,yOff);
+  image(img, 0, 0);  // 在画布上显示更新后的图像
 }
