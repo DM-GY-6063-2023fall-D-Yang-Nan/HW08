b0343ab0abf3551fd7745b6a9e32f435cdc506b9
NanYangEvelyn <ny2187@nyu.edu>
2023-10-30 20:56:20 -0400

    finish2.0

--- 0/sketch.js
+++ 0/sketch.js
@@ -22,42 +22,38 @@ function setup() {
   cp = createColorPicker("#ff0000"); 
   cp.position(10, 10);  
 
-  if (img.width > width) {
+   if (img.width > width) {
      img.resize(width, 0);
-  }
-  if (img.height > height) {
+     }
+     if (img.height > height) {
      img.resize(0, height);
-  }
+     }
     
-  xOff = (width - img.width) / 2;
-  yOff = (height - img.height) / 2;
+     xOff = (width - img.width) / 2;
+     yOff = (height - img.height) / 2;
 }
 
 function draw() {
-  img.copy(origImg, 0, 0, origImg.width, origImg.height, 0, 0, origImg.width, origImg.height);  // Copy the original image into the new image object
+  img.copy(origImg, 0, 0, origImg.width, origImg.height, 0, 0, origImg.width, origImg.height);  // 将原始图像复制到新图像对象中
   
-  let newColor = cp.color();  // Get the current color from the color picker
+  let newColor = cp.color();  // 获取颜色选择器的当前颜色
   
   img.loadPixels();
-  replacementImg.loadPixels();  // Load pixel data for the replacement image
+  replacementImg.loadPixels();  // 加载替换图像的像素数据
   
   for (let i = 0; i < img.pixels.length; i += 4) {
     let r = img.pixels[i];
     let g = img.pixels[i+1];
     let b = img.pixels[i+2];
     
-    // Determine dominant color
-    let maxColorValue = max(r, g, b);
-
-    // If red is the dominant color
-    if (maxColorValue == r) {
-      img.pixels[i] = red(newColor);
-      img.pixels[i+1] = green(newColor);
-      img.pixels[i+2] = blue(newColor);
-    } else if (maxColorValue == b) {  // If blue is the dominant color
-      img.pixels[i] = replacementImg.pixels[i];
-      img.pixels[i+1] = replacementImg.pixels[i+1];
-      img.pixels[i+2] = replacementImg.pixels[i+2];
+    if (r > 80 && g < 100 && b < 100) {  // 调整后的红色检测逻辑
+      img.pixels[i] = red(newColor);   // 替换红色通道
+      img.pixels[i+1] = green(newColor);  // 替换绿色通道
+      img.pixels[i+2] = blue(newColor);  // 替换蓝色通道
+    } else if (r < 100 && g < 100 && b > 90) {  // 蓝色检测逻辑
+      img.pixels[i] = replacementImg.pixels[i];   // 替换图像的红色通道
+      img.pixels[i+1] = replacementImg.pixels[i+1];  // 替换图像的绿色通道
+      img.pixels[i+2] = replacementImg.pixels[i+2];  // 替换图像的蓝色通道
     }
   }
   
--- 1/sketch.js
+++ 1/sketch.js
@@ -9,20 +9,20 @@ function setup() {
  createCanvas(windowWidth,windowHeight);
   amplitude = new p5.Amplitude();
 
-  // 初始化圆的数组
+
   for (let i = 0; i < 50; i++) {
     let x = random(width);
     let y = random(height);
-    let multiplier = random(10, 50);  // 不同的圆有不同的大小变化系数
+    let multiplier = random(10, 50); 
     circles.push(new Circle(x, y, multiplier));
   }
 }
 
 function draw() {
-  background(135,206,250);   //背景色
+  background(135,206,250);  
   let level = amplitude.getLevel();
 
-  // 更新并显示每个圆
+ 
   for (let i = 0; i < circles.length; i++) {
     circles[i].update(level);
     circles[i].display();
@@ -45,16 +45,16 @@ class Circle {
     this.y = y;
     this.multiplier = multiplier;
     this.size = 0;
-    this.alpha = random(255);  // 随机不透明度
+    this.alpha = random(255); 
   }
   
   update(level) {
-    this.size = map(level, 0, 2, 0, 150) * this.multiplier;  // 通过multiplier变量使得每个圆的大小变化不同
+    this.size = map(level, 0, 2, 0, 150) * this.multiplier; 
   }
   
   display() {
-    stroke(240, 235, 222, this.alpha);  // 设置描边颜色和不透明度
-    strokeWeight(10);   //粗細
+    stroke(240, 235, 222, this.alpha);
+    strokeWeight(10); 
     noFill();
     circle(this.x, this.y, this.size);
   }
