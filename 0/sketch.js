93d49aa2a50cec7b4ec76140e4ad720a36d50225
NanYangEvelyn <ny2187@nyu.edu>
2023-10-30 20:58:28 -0400

    update

--- 0/sketch.js
+++ 0/sketch.js
@@ -34,26 +34,26 @@ function setup() {
 }
 
 function draw() {
-  img.copy(origImg, 0, 0, origImg.width, origImg.height, 0, 0, origImg.width, origImg.height);  // 将原始图像复制到新图像对象中
+  img.copy(origImg, 0, 0, origImg.width, origImg.height, 0, 0, origImg.width, origImg.height);  
   
-  let newColor = cp.color();  // 获取颜色选择器的当前颜色
+  let newColor = cp.color(); 
   
   img.loadPixels();
-  replacementImg.loadPixels();  // 加载替换图像的像素数据
+  replacementImg.loadPixels();  
   
   for (let i = 0; i < img.pixels.length; i += 4) {
     let r = img.pixels[i];
     let g = img.pixels[i+1];
     let b = img.pixels[i+2];
     
-    if (r > 80 && g < 100 && b < 100) {  // 调整后的红色检测逻辑
-      img.pixels[i] = red(newColor);   // 替换红色通道
-      img.pixels[i+1] = green(newColor);  // 替换绿色通道
-      img.pixels[i+2] = blue(newColor);  // 替换蓝色通道
-    } else if (r < 100 && g < 100 && b > 90) {  // 蓝色检测逻辑
-      img.pixels[i] = replacementImg.pixels[i];   // 替换图像的红色通道
-      img.pixels[i+1] = replacementImg.pixels[i+1];  // 替换图像的绿色通道
-      img.pixels[i+2] = replacementImg.pixels[i+2];  // 替换图像的蓝色通道
+    if (r > 80 && g < 100 && b < 100) { 
+      img.pixels[i] = red(newColor);  
+      img.pixels[i+1] = green(newColor); 
+      img.pixels[i+2] = blue(newColor);  
+    } else if (r < 100 && g < 100 && b > 90) {  
+      img.pixels[i] = replacementImg.pixels[i];  
+      img.pixels[i+1] = replacementImg.pixels[i+1]; 
+      img.pixels[i+2] = replacementImg.pixels[i+2]; 
     }
   }
   
